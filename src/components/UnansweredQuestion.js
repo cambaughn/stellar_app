f695aa04a05bccb3b4447b610479cfc56466eaf0
Cameron Baughn <cam.baughn@gmail.com>
2017-07-14 21:31:44 -0700

    Add reply icon to unanswered question component

--- src/components/UnansweredQuestion.js
+++ src/components/UnansweredQuestion.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableHighlight, Dimensions } from 'react-native';
 import { NativeRouter, Route, Link, Redirect } from 'react-router-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import colors from '../util/colors';
 
 const UnansweredQuestion = ({ question }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.questionAsker}>
-        <Link to={`/user/${question.asker.id}`} style={styles.link} underlayColor='white'>
-          <Text style={styles.bold}>{question.asker.name}</Text>
+      <View style={styles.topRow}>
+        <View style={styles.questionAsker}>
+          <Link to={`/user/${question.asker.id}`} style={styles.link} underlayColor='white'>
+            <Text style={styles.bold}>{question.asker.name}</Text>
+          </Link>
+          <Text> asks:</Text>
+        </View>
+
+        <Link to={{
+          pathname: `/record_answer/${question.id}`,
+          state: { question: question }
+        }}
+        >
+          {/* <Text style={styles.replyText}>Reply</Text> */}
+          <Icon name={'reply'} style={styles.replyText} />
         </Link>
-        <Text> asks:</Text>
+
       </View>
 
       <Text style={styles.questionText}>{question.text}</Text>
 
-      <Link to={{
-        pathname: `/record_answer/${question.id}`,
-        state: { question: question }
-      }}
-      >
-        <Text style={styles.replyText}>Reply</Text>
-      </Link>
     </View>
   )
 }
@@ -34,11 +40,18 @@ const styles = StyleSheet.create({
 
     paddingTop: 20,
     paddingBottom: 20,
+    paddingRight: 25,
 
     borderBottomWidth: 1,
     borderColor: '#ecf0f1',
 
     width: Dimensions.get('window').width - 20,
+  },
+
+  topRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 
   questionAsker: {
@@ -61,6 +74,7 @@ const styles = StyleSheet.create({
 
   replyText: {
     color: colors.midGrey,
+    fontSize: 15,
   }
 })
 
