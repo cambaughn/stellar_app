1f2dbb15513ef4913cee6e13c31b4050d710c38b
Cameron Baughn <cam.baughn@gmail.com>
2017-08-08 12:34:01 -0700

    Style settings

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 
+import colors from '../../util/colors';
 
 const Settings = ({ user }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{user.profile_photo || 'Photo goes here'}</Text>
-      <Link to={'/'} style={styles.optionLink}>
-        <Text style={styles.text}>{user.name}</Text>
-      </Link>
-        <Text style={styles.text}>{user.email}</Text>
-      <Text style={styles.text}>{user.bio || 'Bio goes here'}</Text>
+      <ScrollView style={styles.scrollView}>
+
+        {/* <Text style={styles.text}>{user.profile_photo || 'Photo goes here'}</Text> */}
+        <Link to={'/'} style={styles.optionLink}>
+          <Text style={styles.text}>{user.name}</Text>
+        </Link>
+
+        <Link to={'/'} style={styles.optionLink}>
+          <Text style={styles.text}>{user.email}</Text>
+        </Link>
+
+        <Link to={'/'} style={styles.optionLink}>
+          <Text style={styles.text}>{user.bio || 'Write your bio!'}</Text>
+        </Link>
+
+      </ScrollView>
     </View>
   )
 }
@@ -21,13 +32,25 @@ const styles = StyleSheet.create({
   // ------------------------ CONTAINER
 
   container: {
-    paddingTop: 100
+  },
+
+  scrollView: {
+    height: '100%',
   },
 
   // ------------------------ LINK
 
   optionLink: {
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.lightGrey,
 
+    paddingLeft: 20,
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
   },
 
   // ------------------------ TEXT
